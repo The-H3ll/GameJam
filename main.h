d6710fe82f6c59a63745a28b10cde49506312424
The-H3ll <44553130+The-H3ll@users.noreply.github.com>
2019-07-05 22:50:23 +0100

    Add files via upload

---
+++ main.h
@@ -0,0 +1,28 @@
+
+/*
+ * Created by M.Labhairi 2019
+ */
+
+
+#ifndef	MAIN_H_
+#define MAIN_H_
+
+/*
+ * Defines and includes
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+# define WIDTH 	800
+# define HEIGHT	600
+# define TRUE	1
+# define FALSE	0
+
+void	init();
+void	pause();
+
+
+#endif
